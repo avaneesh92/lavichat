c77692ca175d5a16ce08cbc78d147dca53578dec
Avaneesh Tripathi <avaneesh92@gmail.com>
2013-08-15 01:30:08 -0700

    chat ready tested on local

--- serve.js
+++ serve.js
@@ -2,29 +2,27 @@
 created on 14/8/2013
 By- Avaneesh Tripathi (avaneesh92@gmail.com)
 */
-var serve = require("./fu");
-var chat = require("./chat");
-var express = require("express");
-var sio = require('socket.io');
 
-var app = express();
-app.use(express.logger());
+var port = process.env.PORT || 5000;
 
-app.get("/", serve.staticHandler("index.html"));
-app.get("/chat",chat.chatHandler);
-var io = sio.listen(app);
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server,{ log: false });
+
+  server.listen(port);
+
+app.get('/', function(request, response){
+  response.sendfile(__dirname + "/index.html");
+});
+
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('message', { msg: 'welcome to the chat' });
     socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+        socket.broadcast.emit('message', data);
     });
 });
