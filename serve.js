e481001f1ad4e862a4d07614b81bff15566221c3
Avaneesh Tripathi <avaneesh92@gmail.com>
2013-08-15 03:25:26 -0700

    Added logins

--- serve.js
+++ serve.js
@@ -4,15 +4,28 @@ By- Avaneesh Tripathi (avaneesh92@gmail.com)
 */
 
 var port = process.env.PORT || 5000;
-
+var express=require('express');
+var pass="pentium@5192126";
 var app = require('express')()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server,{ log: false });
+  , io = require('socket.io').listen(server,{ log: true});
+
+ app.use(express.bodyParser()); 
 
   server.listen(port);
-
 app.get('/', function(request, response){
-  response.sendfile(__dirname + "/index.html");
+  response.sendfile(__dirname + "/login.html");
+});
+app.get('/beep.wav', function(request, response){
+  response.sendfile(__dirname + "/beep.wav");
+});
+app.post('/index.html', function(request, response){
+  if(request.body.pass===pass){
+      response.sendfile(__dirname + "/index.html");
+	  }
+  else{
+      response.sendfile(__dirname + "/login.html");
+      }	  
 });
 
 io.configure(function () { 
